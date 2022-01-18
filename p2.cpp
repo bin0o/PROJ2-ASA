e78948d0287aedfa70e0544c4997fdef055bc389
caldas479 <tiago.rodrigues.caldas@gmail.com>
2022-01-18 13:02:39 +0000

    input feito

--- p2.cpp
+++ p2.cpp
@@ -2,6 +2,7 @@
  * IST: ASA 2021/22 - P2 - Manuel Albino, Tiago Caldas                        *
  ******************************************************************************/
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -13,17 +14,45 @@ int v2;
 int n;
 int m;
 
-void readInput(int(&graph) [][EDGE_SIZE]) {
+void readInput(vector<int> adj[]) {
+    for(int i = 0; i < m; i++){
+        int x;
+        int y;
+        scanf("%d %d", &x, &y);
+        adj[x].push_back(y);
+    }
+}
 
+int verifyTree(vector<int> adj[]){
+    for(int v = 1; v <= n; v++){
+        for(auto u : adj[v]){
+            if (u == v){
+                return -1;
+            }
+        }
+        printf("\n");
+    }
+    return 1;
+}
 
+int printTree(vector<int> adj[]){
+    for(int v = 1; v <= n; v++){
+        cout << "Head[" << v <<"]";
+        for(auto u : adj[v]){
+            cout << "-> [" << u <<"] ";
+        }
+        printf("\n");
+    }
+    return 1;
 }
 
 int main() {
 
-    cin >> v1 >> v2;
-    cin >> n >> m;
-    int graph[m][EDGE_SIZE];
-    readInput(graph);
+    scanf("%d %d", &v1, &v2);
+    scanf("%d %d", &n, &m);
+    vector<int> adj[n+1];
+    readInput(adj);
+    verifyTree(adj);
 
     return 0;
 }
